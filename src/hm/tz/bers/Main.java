5e4ada092319bac6077987537730008bd271194e
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 13:26:49 +0300

    Добавлена опция "Количество приборов"

--- src/hm/tz/bers/Main.java
+++ src/hm/tz/bers/Main.java
@@ -7,15 +7,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int time;
+        int time, amount;
         double price, power, result;
         System.out.println("Введите стоимость за КВт/ч:");
         price = input.nextDouble();
-        System.out.println("Введите время работы в часах за день:");
+        System.out.println("Введите время работы в часах за рабочий день:");
         time = input.nextInt();
-        System.out.println("Введите мощность прибора:");
+        System.out.println("Введите количество приборов:");
+        amount = input.nextInt();
+        System.out.println("Введите мощность прибора(ов):");
         power = input.nextDouble();
-        result = (((((power / 1000))*(time*30)*price)));
+        result = ((((((power / 1000))*(time*30)*price))))*amount;
         System.out.println("За месяц вы заплатите: " + result + " рублей");
 
     }
