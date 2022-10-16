d73b0d2a9513eef9bbabc9461823f180087754bf
berserkzak <me4anicman@gmail.com>
2022-10-16 19:44:37 +0300

    Версия 0.3.0 Добавлен выбор типа электротарификации Добавлена MIT лицензия Изменения в readme.md Изменения .gitignore Небольшие изменения по оформлению кода
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/hm/tz/ep/Main.java
+++ src/hm/tz/ep/Main.java
@@ -5,28 +5,51 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double result;
-        String name;
+
         try (Scanner input = new Scanner(System.in)) {
 
             int amount;
-            String answertwo;
-            double price, power, time;
+            String answer_lang, answer_type, name;
+            double price, price_night, power, time, time_night, result;
 
-            System.out.println("Напишите q для русского языка либо e для английского.");
-            answertwo = input.nextLine();
-            if ("q".equalsIgnoreCase(answertwo)) {
-                System.out.println("Введите стоимость за КВт/ч:");
-                price = input.nextDouble();
-                System.out.println("Введите название прибора:");
-                name = input.next();
-                System.out.println("Введите количество приборов:");
-                amount = input.nextInt();
-                System.out.println("Введите мощность прибора(ов) в Ваттах:");
-                power = input.nextDouble();
-                System.out.println("Введите время работы в часах за рабочий день:");
-                time = input.nextDouble();
-                result = ((((((power / 1000)) * (time * 30) * price)))) * amount;
+            System.out.println("Выберите язык:");
+            System.out.println("1 - Русский");
+            System.out.println("2 - Английский");
+            answer_lang = input.nextLine();
+            if ("1".equalsIgnoreCase(answer_lang)) {
+                System.out.println("Выберите тип электротарификации:");
+                System.out.println("1 - Одноставочный");
+                System.out.println("2 - Двухставочный");
+                answer_type = input.nextLine();
+                if ("1".equalsIgnoreCase(answer_type)) {
+                    System.out.println("Введите стоимость за КВт/ч:");
+                    price = input.nextDouble();
+                    System.out.println("Введите название прибора:");
+                    name = input.next();
+                    System.out.println("Введите количество приборов:");
+                    amount = input.nextInt();
+                    System.out.println("Введите мощность прибора(ов) в Ваттах:");
+                    power = input.nextDouble();
+                    System.out.println("Введите время работы в часах за рабочий день:");
+                    time = input.nextDouble();
+                    result = ((((((power / 1000)) * (time * 30) * price)))) * amount;
+                } else {
+                    System.out.println("Введите стоимость за КВт/ч по дневной зоне:");
+                    price = input.nextDouble();
+                    System.out.println("Введите стоимость за КВт/ч по ночной зоне:");
+                    price_night = input.nextDouble();
+                    System.out.println("Введите название прибора:");
+                    name = input.next();
+                    System.out.println("Введите количество приборов:");
+                    amount = input.nextInt();
+                    System.out.println("Введите мощность прибора(ов) в Ваттах:");
+                    power = input.nextDouble();
+                    System.out.println("Введите время работы в часах с 7 утра до 23 вечера:");
+                    time = input.nextDouble();
+                    System.out.println("Введите время работы в часах с 23 вечера до 7 утра:");
+                    time_night = input.nextDouble();
+                    result = ((((((power / 1000)) * ((time * 30) * price)))) + ((((((power / 1000)) * ((time_night * 30) * price_night)))))) * amount;
+                }
                 System.out.println("За " + name + " в месяц вы заплатите: " + result + " рублей(я).");
             } else {
                 System.out.println("Write price for KWh/h:");
@@ -44,7 +67,6 @@ public class Main {
             }
 
 
-
-    }
+        }
 
 }}
